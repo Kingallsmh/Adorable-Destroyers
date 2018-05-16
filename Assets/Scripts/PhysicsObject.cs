82eb2e50e1ccfb500231fe81302942fc1a0ea3ed
Kyle <kyle@langalab.com>
2018-05-16 16:45:48 -0300

    Many Models plus pickup

---
+++ Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+
+    public float minGroundNormalY = .65f;
+    public float gravityModifier = 1f;
+    public float maxVerticalSpeed = 10;
+    protected Vector2 targetVelocity;
+    protected bool grounded;
+    protected bool unphysics;
+    protected bool isBusy = false;
+    protected Vector2 groundNormal;
+    protected Rigidbody2D rb2d;
+    protected Vector2 velocity;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
+
+
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.01f;
+
+    void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        contactFilter.useLayerMask = true;
+    }
+
+    void Update()
+    {
+        if (!unphysics)
+        {
+            targetVelocity = Vector2.zero;
+            ComputeVelocity();
+        }
+    }
+
+    protected virtual void ComputeVelocity()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (unphysics)
+        {
+            velocity = Vector2.zero;
+            return;
+        }
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        velocity.x = targetVelocity.x;
+        velocity.y = Mathf.Clamp(velocity.y, -maxVerticalSpeed, maxVerticalSpeed);
+
+        grounded = false;
+
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+
+        Vector2 moveAlongGround = new Vector2(groundNormal.y, -groundNormal.x);
+
+        Vector2 move = moveAlongGround * deltaPosition.x;
+
+        //Debug.Log("Current normal: " + groundNormal);
+
+        Movement(move, false);
+
+        move = Vector2.up * deltaPosition.y;
+
+        Movement(move, true);
+    }
+
+	void Movement2(Vector3 move, bool yMovement){
+		Rigidbody rb;
+		int count = Physics.BoxCastNonAlloc(rb.position, rb.)
+	}
+
+    protected void Movement(Vector2 move, bool yMovement)
+    {
+        float distance = move.magnitude;
+
+        if (distance > minMoveDistance)
+        {
+			
+            int count = rb2d.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
+
+            Collider2D[] rbC = new Collider2D[1];
+            rb2d.GetAttachedColliders(rbC);
+            Collider2D rbCol = rbC[0];
+            Vector2 pos = new Vector2(rbCol.bounds.center.x, rbCol.bounds.center.y - rbCol.bounds.extents.y);
+            Debug.DrawRay(pos, move * 100, Color.blue);
+            hitBufferList.Clear();
+            for (int i = 0; i < count; i++)
+            {
+                PlatformEffector2D platform = hitBuffer[i].collider.GetComponent<PlatformEffector2D>();
+                PlatformAddition platformAddition = hitBuffer[i].collider.GetComponent<PlatformAddition>();
+                if (!platform)
+                {
+                    hitBufferList.Add(hitBuffer[i]); // get the colliding objects
+                }
+                else if ((velocity.y < 0 && yMovement))
+                { //hitBuffer[i].normal == Vector2.up && 
+                    if (hitBuffer[i].normal.y > 0)
+                    {
+                        //Debug.Log(hitBuffer[i].normal);
+                        if (rb2d.IsTouching(hitBuffer[i].collider))
+                        {
+                            hitBufferList.Add(hitBuffer[i]);
+                        }
+                    }
+                }
+                if (platformAddition)
+                {
+                    if (platformAddition.MoveWithPlatform)
+                    {
+                        //Debug.Log("Platform Move: " + platformAddition.rb2d.velocity);
+                        rb2d.velocity += hitBuffer[i].collider.GetComponent<PlatformAddition>().rb2d.velocity;
+                    }
+                }
+            }
+
+            for (int i = 0; i < hitBufferList.Count; i++)
+            {
+                Vector2 currentNormal = hitBufferList[i].normal;
+                if (currentNormal.y > minGroundNormalY)
+                {
+                    grounded = true;
+                    if (yMovement)
+                    {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(velocity, currentNormal);
+                if (projection < 0)
+                {
+                    velocity = velocity - projection * currentNormal;
+                }
+
+                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+
+        }
+        rb2d.position = rb2d.position + move.normalized * distance;
+    }
+
+    public void ResetGroundNormal()
+    {
+        groundNormal = new Vector2(0, 1);
+    }
+
+}
--- Assets/Scripts/ThirdPersonControl.cs
+++ Assets/Scripts/ThirdPersonControl.cs
@@ -8,7 +8,14 @@ public class ThirdPersonControl : MonoBehaviour {
 	Animator anim;
 	Rigidbody rb;
 	public float speed;
+	public float jumpSpeed;
 	Transform trans;
+
+    //For grabbing and throwing
+	public Collider myCol;
+	public float throwSpeed;
+	public float pickupThrowDelay = 0.2f;
+	GameObject heldObject;
 
 
 	// Use this for initialization
@@ -17,25 +24,44 @@ public class ThirdPersonControl : MonoBehaviour {
 		trans = obj.transform;
 		rb = GetComponent<Rigidbody>();
 		anim = GetComponent<Animator>();
+		StartCoroutine(CharacterLoop());
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		Movement();
+	//void Update () {
+	//	Movement();
+	//	PickupObject(myCol);
+	//	ThrowObject();
+	//}
+
+	IEnumerator CharacterLoop(){
+		while(true){
+			Movement();
+			if (Input.GetKeyDown(KeyCode.Z))
+			{
+				if(!heldObject){
+					yield return StartCoroutine(PickupObject(myCol));
+				}
+				else{
+					yield return StartCoroutine(ThrowObject());
+				}
+			}
+			yield return null;
+		}
 	}
 
 	Vector3 GetDirectionInput()
     {
 		//input = control.MoveInput;
 		//input *= speed * Time.deltaTime;
-		return new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * speed * 10 * Time.deltaTime;
+		return new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * speed * 10 * Time.deltaTime;
     }
 
 	void Movement(){
 		trans.position = Camera.main.transform.position;
 		trans.rotation = Camera.main.transform.rotation;
 		trans.eulerAngles = new Vector3(0, trans.rotation.eulerAngles.y, 0);
-		//Jump();
+		Jump();
 		//if (input.y > -maxVelChange)
 		//{
 		//    input.y += currentGravity;
@@ -56,6 +82,12 @@ public class ThirdPersonControl : MonoBehaviour {
         //isGrounded = false;
 	}
 
+	void Jump(){
+		if(Input.GetKeyDown(KeyCode.Space)){
+			rb.AddForce(0, jumpSpeed * 10, 0);
+		}
+	}
+
 	void FaceTowardsDirection(Vector3 direction)
     {
         //If rotation in minute in difference, don't bother rotating
@@ -67,4 +99,26 @@ public class ThirdPersonControl : MonoBehaviour {
             }
         }
     }
+
+	IEnumerator PickupObject(Collider col){
+		RaycastHit hit;
+		if(Physics.BoxCast(col.transform.position, col.bounds.extents, transform.forward, out hit, transform.rotation, 1)){
+			hit.transform.parent = transform;
+			hit.transform.GetComponent<Rigidbody>().useGravity = false;
+			hit.transform.GetComponent<Rigidbody>().isKinematic = true;
+			heldObject = hit.transform.gameObject;
+			yield return new WaitForSeconds(pickupThrowDelay);
+		}
+	}
+
+	IEnumerator ThrowObject(){
+		if(heldObject){
+			heldObject.GetComponent<Rigidbody>().useGravity = true;
+			heldObject.GetComponent<Rigidbody>().isKinematic = false;
+			heldObject.GetComponent<Rigidbody>().AddForce(transform.forward * throwSpeed);
+			heldObject.transform.parent = null;
+			heldObject = null;
+			yield return new WaitForSeconds(pickupThrowDelay);
+		}
+	}
 }
